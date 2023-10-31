f216d041994f381ff0d233b90a823aef6e36f859
ulmas <olmasbek.rakhmatullaev@eriell.com>
2023-10-31 17:18:16 +0500

    feat: add no internet error handling

--- lib/util/network/base_api_handler.dart
+++ lib/util/network/base_api_handler.dart
@@ -1,10 +1,18 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:uboyniy_cex/util/util.dart';
 
 class BaseApiHanlder {
   static Future<T> request<T>(Future<T> Function() request) async {
     try {
-      return await request();
+      final connectivityResult = await Connectivity().checkConnectivity();
+      if (connectivityResult == ConnectivityResult.wifi ||
+          connectivityResult == ConnectivityResult.mobile ||
+          connectivityResult == ConnectivityResult.ethernet) {
+        return await request();
+      } else {
+        throw NoInternetException();
+      }
     } on DioException catch (e) {
       switch (e.type) {
         case DioExceptionType.badResponse:
@@ -21,6 +29,8 @@ class BaseApiHanlder {
         case _:
           throw UnkonwnException(statusCode: e.response?.statusCode);
       }
+    } on NoInternetException {
+      rethrow;
     } catch (e) {
       throw UnkonwnException();
     }
